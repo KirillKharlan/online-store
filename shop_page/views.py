faf6be8d1022fce4ec1491fadbf5e107c04ed208
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2024-06-19 13:14:16 +0300

    online-store

--- shop_page/views.py
+++ shop_page/views.py
@@ -5,7 +5,39 @@ import pandas
 from project.settings import DATABASE
 from registration_page.models import User, Product
 from .app import shop
+from PIL import Image
 def render_shop_page():
+    mod = False
+    type1 = None
+    id = None
+    if flask.request.method == "POST":
+        try:
+        
+            if flask.request.form["send"].split(";")[1] == "IMG":
+            
+                print(flask.request.form["send"])
+                print(os.path.abspath(__file__+"/../static/image"))
+                
+                img = flask.request.files.get('image')
+                print(img, type(img))
+                # image = Image.open(img)
+                # print(image, type(image))
+                # image.show()
+            elif flask.request.form["send"].split(";")[1] == "TEXT":
+                text = flask.request.form["text"]
+                id = flask.request.form["send"].split(";")[0]
+                for product in Product.query.all():
+                    if product.id == id:
+                        product.name = text
+        except :
+            try:
+                print(flask.request.form['img'])
+                id = flask.request.form['img']
+                type1 = "IMG"
+                
+            except:
+                pass
+            mod = True
     try:
         count =  len(flask.request.cookies.get('products').split(" "))
         if flask.request.cookies.get('products').split(" ")[0]== "":
@@ -38,13 +70,19 @@ def render_shop_page():
     # for product in User.query.all():
     # if count == None:
     #     count = '0'
+    list_admins=["Illya","Mykola Skrypnik"]
+    print()
+    admin=flask_login.current_user.login in list_admins
     cookie = flask.make_response(
         flask.render_template(template_name_or_list="shop.html",
                               name=flask_login.current_user.login, 
                               products = Product.query.all(),
                               count = count,
                               int = int,
-                              admin = False
+                              admin =admin,
+                              type = type1,
+                              mod = mod,
+                              id = id
                               ))
     # cookie.set_cookie("all", count)
     
